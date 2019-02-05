9552d14fe35080df7b915ca2ffc0d0d2ff643f8a
Shailesh Pant <shailesh.pant@corp.ovh.net>
2019-02-06 00:24:43 +0530

    - Added GetChain feature that return immutable string representation of the chain

--- main.go
+++ main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"log"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/heckdevice/gobc/core"
 )
 
 func main() {
-	for i := 0; i < 100; i++ {
+	testAdd()
+}
+
+func testAdd() {
+	for i := 0; i < 2; i++ {
 		_, err := core.Add(map[string]interface{}{"DataPoint": i})
 		if err != nil {
 			log.Fatal(err)
 		}
+	}
+	bcData, err := core.GetChain()
+	if err != nil {
+		log.Fatal(err)
+
+	} else {
+		spew.Dump(bcData)
 	}
 }
