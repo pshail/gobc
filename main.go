a137778ef2d345555d32e1e8d9575f6086e680f6
Shailesh Pant <shailesh.pant@corp.ovh.net>
2019-02-06 15:34:25 +0530

    - Added webservice to GET / POST data to the blockchain  - Added utils function InterfaceToJSONString

--- main.go
+++ main.go
@@ -3,17 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/heckdevice/gobc/core"
+	"github.com/heckdevice/gobc/webservice"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	testAdd()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testRun := os.Getenv("TEST_RUN")
+	if testRun == "true" {
+		testAdd()
+	} else {
+		initGenesisBlock()
+		log.Fatal(webservice.Run())
+	}
 }
 
+func initGenesisBlock() {
+	//Initialize the genesis block
+	go func() {
+		core.Init()
+	}()
+}
 func testAdd() {
 	for i := 0; i < 10000; i++ {
-		_, err := core.Add(map[string]interface{}{"DataPoint": i})
+		_, _, err := core.Add(map[string]interface{}{"DataPoint": i})
 		if err != nil {
 			log.Fatal(err)
 		}
